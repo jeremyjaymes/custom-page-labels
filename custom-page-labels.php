2bf44dae44073987c54623a53da6a8e4fc6fcfca
Jeremy <jeremy@papertreedesign.com>
2014-06-19 17:08:18 -0500

    Modifying plugin header to work with deploy script.

--- custom-page-labels.php
+++ custom-page-labels.php
@@ -1,32 +1,32 @@
 <?php
-/**
- * Plugin Name: Custom Page Labels
- * Plugin URI: https://github.com/jeremyjaymes/custom-page-labels
- * Description: Custom page label admin column and metabox
- * Version: 0.1.0
- * Author: jeremyjaymes
- * Author URI: http://jeremyjaymes.com
- * Text Domain: pk_custom_page_label
- * Domain Path: /languages
- *
- * License: GPLv2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- *
- * Copyright 2014 Jeremy Vossman (email : jeremy@papertreedesign.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- */
+/*
+Plugin Name: Custom Page Labels
+Plugin URI: https://github.com/jeremyjaymes/custom-page-labels
+Description: Custom page label admin column and metabox
+Version: 0.1.0
+Author: jeremyjaymes
+Author URI: http://jeremyjaymes.com
+Text Domain: pk_custom_page_label
+Domain Path: /languages
+
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright 2014 Jeremy Vossman (email : jeremy@papertreedesign.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 
 // If this file is called directly, abort.
